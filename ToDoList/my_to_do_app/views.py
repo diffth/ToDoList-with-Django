fd5c8ba2948bfe9bd5fa848d7ebd1bda31c1e709
diffth <diffth@nate.com>
2020-01-30 16:47:46 +0900

    6th CRUD -CR

--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
+    todos = Todo.objects.all()
+    contentDic = {'todos':todos}
+    return render(request, 'my_to_do_app/index.html', contentDic)
     # return HttpResponse("my_to_do_app first page")
-    return render(request, 'my_to_do_app/index.html')
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("create Todo를 할꺼야 => " + user_input_str)
+    # return HttpResponse("create Todo를 할꺼야!")
